136f7c17f5d7e76331f06a1d67604c0825359b59
Adjetey Sowah <adjetey.07@live.com>
2017-07-29 11:00:20 +0000

    Fixed category filter

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,4 +4,22 @@ $(function() {
      effectTime: 2000,
      threshold: 0
    });
+
+   // init Isotope
+   var $grid = $('.grid').isotope({
+     // options
+      itemSelector: '.grid-item',
+      layoutMode: 'fitRows'
+   });
+   // filter items on button click
+   $('.filter-button-group').on( 'click', 'a', function() {
+     var filterValue = $(this).attr('data-filter');
+     $grid.isotope({ filter: filterValue });
+   });
+
+   $('.button-group a.button').on('click', function(){
+    $('.button-group a.button').removeClass('active');
+    $(this).addClass('active');
+   });
 });
+
